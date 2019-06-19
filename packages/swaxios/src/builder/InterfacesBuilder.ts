223927a44e154dd850815bdd4c378645c67f5015
Florian Keller <florian.keller@wire.com>
2019-06-19 17:19:30 +0200

    add boolean

--- packages/swaxios/src/builder/InterfacesBuilder.ts
+++ packages/swaxios/src/builder/InterfacesBuilder.ts
@@ -89,6 +89,9 @@ export class InterfacesBuilder {
       case SwaggerType.STRING: {
         return TypeScriptType.STRING;
       }
+      case SwaggerType.BOOLEAN: {
+        return TypeScriptType.BOOLEAN;
+      }
       case SwaggerType.NUMBER:
       case SwaggerType.INTEGER: {
         return TypeScriptType.NUMBER;
